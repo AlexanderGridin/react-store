2088521faa303fe3a7ebb5c22d79ea1b693e974c
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2022-12-18 23:52:04 +0200

    app component updated

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,20 @@ import "./App.css";
 
 import Container from "../shared/components/container/Container";
 import Logo from "../shared/components/logo/Logo";
-import Phone from "../shared/components/phone/Phone";
+import CallbackButton from "../shared/components/callback-button/CallbackButton";
+import ContactLink, {
+  ContactLinkType,
+} from "../shared/components/contact-link/ContactLink";
+import CartButton from "../shared/components/cart-button/CartButton";
 
 const App = (): JSX.Element => {
   return (
     <>
       <Container>
         <Logo />
-        <Phone phone="+38 (063) 123 45 67" />
+        <CallbackButton />
+        <ContactLink type={ContactLinkType.Tel} value="+38 (063) 123 45 67" />
+        <CartButton totalProducts={3}></CartButton>
       </Container>
     </>
   );
