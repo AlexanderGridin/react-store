dc58f1999a86bd487dc7e7c3c6a06f685c985fc7
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2022-12-19 14:42:51 +0200

    transform logo to a link

--- src/shared/components/logo/Logo.tsx
+++ src/shared/components/logo/Logo.tsx
@@ -1,11 +1,13 @@
-import logo from '../../../assets/logo.svg';
-import './Logo.css';
+import logo from "../../../assets/logo.svg";
+import "./Logo.css";
 
 const Logo = (): JSX.Element => {
-	return (<div className='Logo'>
-		<img className='Logo__icon' src={logo} alt="Company logo"/>
-		<p className='Logo__text'>Womazing</p>
-	</div>)
-}
+  return (
+    <a className="Logo" href="/">
+      <img className="Logo__icon" src={logo} alt="Company logo" />
+      <p className="Logo__text">Womazing</p>
+    </a>
+  );
+};
 
 export default Logo;
